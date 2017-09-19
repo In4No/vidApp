8fa3490c6a6962edb2a5c818e86ff6f11e71eb65
Pratik <pratik@inkoniq.com>
2017-09-19 17:05:11 +0530

    video capture fixed

--- App.js
+++ App.js
@@ -12,13 +12,57 @@ let videoDimensions = width - 40
 export default class App extends React.Component {
   constructor(props){
     super(props)
+    console.log("here")
     this.state = {
-      cam : false
+      cam : false,
+      captureOn : false,
+      capturing : false
     }
   }
 
   showCam = () => {
-    this.setState({cam : !this.state.cam})
+    let camstate = this.state.cam
+    let captureState = this.state.captureOn
+    if(this.state.capturing){
+      console.log("stopped capture")
+      this.camera.stopCapture()
+      this.setState({capturing : false})
+    }
+    if(this.state.captureOn){
+      console.log("started capture")
+      this.setState({capturing : true})
+      const options = {};
+      options.totalSeconds = 10
+      this.camera.capture({metadata: options})
+      .then((data) => console.log(data))
+      .catch(err => console.error(err));
+    }
+    if(!camstate){
+      this.setState({cam : true})
+    }else{
+      this.setState({captureOn : !captureState})
+    }
+  }
+
+  takeBack = () => {
+    this.setState({cam : false, captureOn : false})
+  }
+
+  getButtonStyle = () => {
+    if(this.state.cam){
+      if(this.state.captureOn){
+        return styles.vidButtonStop
+      }else{
+        return styles.vidButtonLive
+      }
+    }else{
+      return styles.vidButton
+    }
+  }
+
+  uploadVid = () => {
+    console.log("will upload")
+    // run upload function
   }
 
   getInner = () => {
@@ -39,7 +83,7 @@ export default class App extends React.Component {
               />
             </View>
             <Text style={styles.userName}>John Doe</Text>
-            <TouchableOpacity onPress={()=>this.showCam()}><Text style={{color: '#666666' }}>Show Cam</Text></TouchableOpacity>
+            <TouchableOpacity onPress={()=>this.showCam()} style={{width: width-40, height: 80, alignItems: 'center', justifyContent: 'center'}}><View style={this.getButtonStyle()}></View></TouchableOpacity>
           </View>
           <View style={styles.miniWrpr}>
             <View style={styles.videoContainer}>
@@ -75,7 +119,31 @@ export default class App extends React.Component {
       )
     }else{
       return(
-        <View style={styles.camWrapper}></View>
+        <View style={styles.camWrapper}>
+          <Camera
+            ref={(cam) => {
+              this.camera = cam;
+            }}
+            style={styles.cameraPreview} 
+            aspect={Camera.constants.Aspect.fill} 
+            type={"front"} 
+            playSoundOnCapture={false} 
+            captureMode={Camera.constants.CaptureMode.video} 
+            totalSeconds={15} 
+            >
+          </Camera>
+          <View style={styles.controlWrpr}>
+            <TouchableOpacity onPress={()=>this.takeBack()} style={styles.backWrpr}>
+              <Text style={styles.backText}>BACK</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={()=>this.showCam()} style={styles.recordWrpr}>
+              <View style={this.getButtonStyle()}></View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={()=>this.uploadVid()} style={styles.backWrpr}>
+              <Text style={styles.backText}>UPLOAD</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       )
     }
   }
@@ -101,9 +169,11 @@ const styles = StyleSheet.create({
     width: width,
     height: height,
     alignItems: 'center',
-    justifyContent: 'center',
-    borderWidth: 1,
-    borderColor: 'red'
+    justifyContent: 'center'
+  },
+  cameraPreview:{
+    width: videoDimensions,
+    height: videoDimensions,
   },
   miniWrpr:{
     padding: 20,
@@ -120,5 +190,50 @@ const styles = StyleSheet.create({
   userName:{
     fontSize: 40,
     color: '#666666'
-  }
+  },
+  vidButton:{
+    width: 40,
+    height: 40,
+    backgroundColor: '#ffffff',
+    borderRadius: 20,
+    borderWidth: 2,
+    borderColor: 'red'
+  },
+  vidButtonLive:{
+    width: 42,
+    height: 42,
+    backgroundColor: 'red',
+    borderRadius: 20,
+    borderWidth: 2,
+    borderColor: 'red'
+  },
+  vidButtonStop:{
+    width: 38,
+    height: 38,
+    backgroundColor: '#ffffff',
+    borderWidth: 4,
+    borderColor: 'red'
+  },
+  controlWrpr:{
+    width: width - 40,
+    height: 60,
+    justifyContent: 'center',
+    flexDirection: 'row',
+    alignContent: 'space-between',
+    borderColor: 'red',
+    borderWidth: 1,
+    marginTop: 100
+  },
+  backWrpr:{
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#666666',
+    marginBottom: 20
+  },
+  recordWrpr:{
+    width: 50,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
 });
